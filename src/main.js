1271e693b60ce991107a32a26adc3cf4b2e2ba97
MalumRubiginous <malum0929@hotmail.com>
2019-10-29 17:18:53 +0800

    init vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+
+import store from './store/index.js';
 import App from './App';
 
 const vm = new Vue({
     el: '#root',
+    store,
     render: (createElement) => createElement(App)
 });
